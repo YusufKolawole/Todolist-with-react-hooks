a44fc69fd60af556a5b607734a80f429eb49eabc
Yusuf Quadri Kolawole <Quadyusuf@gmail.com>
2020-02-07 14:57:01 +0100

    writing TodoInput

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React, {useState} from 'react'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+function TodoInput(){
+  const [todoInput, setTodoInput] = useState(" ");
+  return <input placeholder="type here" onChange={(e)=>{
+    setTodoInput(e.target.value);
+    console.log(todoInput)
+  }}/>
+  
+}
 
-
-
-
-export default App;
+export default TodoInput;
