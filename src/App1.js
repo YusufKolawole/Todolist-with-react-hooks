537905c45ccf9adea0eae65e5cb0eb3f16d2a81a
Yusuf Quadri Kolawole <Quadyusuf@gmail.com>
2020-02-08 13:51:14 +0100

    implementing useReducer equivalent in classical components

--- src/App1.js
+++ src/App1.js
@@ -6,8 +6,22 @@ export default class TodoInput extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      todoInput : '',
-    }
+      todos : JSON.parse(localStorage.getItem("todos"))  || []
+    };
+    this.handleRemoveItem = this.handleRemoveItem.bind(this);
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
+
+  handleRemoveItem(id){
+      const newTodos = this.state.todos.filter(todo => todo.id !== id);
+      this.setState({todos : [...newTodos]});
+      localStorage.setItem("todos", JSON.stringify(this.state.todos));
+  }
+
+   handleAddTodo(todo) {
+    if (this.state.todos) this.setState({ todos: [...this.state.todo, todo] });
+    else this.setState({ todos: [todo] });
+    localStorage.setItem("todos", JSON.stringify(this.state.todos));
   }
   render(){
     return(
