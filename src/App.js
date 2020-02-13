c1dbc906a6d961c36ea87074989b55144ee5044f
Yusuf Quadri Kolawole <Quadyusuf@gmail.com>
2020-02-13 22:50:19 +0100

    real todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import TodoInput from "./App1"
-import TodoInput2 from "./App2"
-export default function App(){
-  return(
-    <React.Fragment>
-        <TodoInput/>
-        <TodoInput2/>
-    </React.Fragment>
-  )
-}
+import
--- src/App2.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-
-export default function TodoInput2() {
-  const [todoInput, setTodoInput] = useState('');
-  return <input placeholder="With Functional componnent" onChange={(e) => setTodoInput(e.target.value)} />
-}
-
