abd85eb0b8c2dffb27d757b8e9e30b63c3753bd3
Yusuf Quadri Kolawole <Quadyusuf@gmail.com>
2020-02-15 00:29:58 +0100

    done with the todo with react-hooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,73 @@
 import React, {useState} from 'react';
 import "./App.css"
-function Todo({todo, index}){
+function Todo({todo, index, completeTodo, removeTodo}){
    return(
-      <div className="todo">
+      <div style={{textDecoration:todo.isCompleted ? "line-through" : ''}} className="todo">
          {todo.text}
+         <div>
+            <button onClick={()=> completeTodo(index)}>Complete</button>
+            <button onClick={()=> removeTodo(index)}>x</button>
+         </div>
       </div>
    )
 }
 
+function TodoForm({addTodo}){
+   const [value, setValue] = useState('');
+
+   const handleSubmit = e => {
+      e.preventDefault();
+      if(!value)return;
+      addTodo(value);
+      setValue('')
+   }
+   return (
+      <form onSubmit={handleSubmit}>
+      <input className="input" value={value} onChange={e=>setValue(e.target.value)} placeholder="add todo"/>
+   </form>
+   )
+}
+
 function App(){
-    const [todos, setTodos] = useState([
+    const [todos, setTodos] = useState([ 
       {
          text: 'Learn advance react',
-         isComplete : false,
+         isCompleted : false,
       },
       {
         text: 'Build  social media',
-        isComplete : false
+        isCompleted : false
      },
      {
       text: 'Startup later',
-      isComplete : false
+      isCompleted : false
    }
-    ])
-    return (
+    ]);
+
+    const addTodo = text => {
+       const NewTodos = [...todos, {text}];
+       setTodos(NewTodos)
+
+    }
+
+    const completeTodo = (index)=>{
+         const newTodos = [...todos];
+         newTodos[index].isCompleted = true;
+         setTodos(newTodos)
+    }
+
+    const removeTodo = (index)=>{
+         var newTodos = [...todos];
+         newTodos.splice(index, 1);
+         setTodos(newTodos)
+    }
+    return ( 
        <div className="app">
           <div className="todo-list">
             {todos.map((todo, index)=>(
-               <Todo key={index} index={index} todo={todo}/>
+               <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} removeTodo={removeTodo}/>
             ))}
+            <TodoForm addTodo={addTodo}/>
           </div>
        </div>
     )
