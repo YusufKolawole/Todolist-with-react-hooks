a9aee2dd370dbb4d501afcf93d8f9970506fe839
Yusuf Quadri Kolawole <Quadyusuf@gmail.com>
2020-02-14 21:21:34 +0100

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,1 +1,38 @@
-import
+import React, {useState} from 'react';
+import "./App.css"
+function Todo({todo, index}){
+   return(
+      <div className="todo">
+         {todo.text}
+      </div>
+   )
+}
+
+function App(){
+    const [todos, setTodos] = useState([
+      {
+         text: 'Learn advance react',
+         isComplete : false,
+      },
+      {
+        text: 'Build  social media',
+        isComplete : false
+     },
+     {
+      text: 'Startup later',
+      isComplete : false
+   }
+    ])
+    return (
+       <div className="app">
+          <div className="todo-list">
+            {todos.map((todo, index)=>(
+               <Todo key={index} index={index} todo={todo}/>
+            ))}
+          </div>
+       </div>
+    )
+
+}
+
+export default App;    
